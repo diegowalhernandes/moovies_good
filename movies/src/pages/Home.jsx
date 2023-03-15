df383703488e9441568225e38506ce929b9fbd11
diego hernandes <diegowalhernandes@gmail.com>
2023-03-15 15:00:21 -0300

    .

--- movies/src/pages/Home.jsx
+++ movies/src/pages/Home.jsx
@@ -1,7 +1,27 @@
-const Home = () => {
-    return <div>Home
+import { useState, useEffect } from "react";
 
-    </div>;
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+    const [topMovies, setTopMovies] = useState([]);
+
+        const getTopRatedMOvies = async (url) => {
+	    const res = await fetch(url);
+    	const data = await res.json();
+	
+	setTopMovies(data.results);
+};
+
+    useEffect(() => {
+	const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+        getTopRatedMOvies(topRatedUrl);
+}, []);
+    return(
+        <div>{topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}</div>
+    );
+     
 };
 
 export default Home
