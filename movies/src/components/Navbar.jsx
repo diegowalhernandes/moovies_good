df44f4617cdcb24d56957b66c1ce929831c02bc9
diego hernandes <diegowalhernandes@gmail.com>
2023-03-16 20:30:25 -0300

    ...

--- movies/src/components/Navbar.jsx
+++ movies/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+
+import "./Navbar.css";
 const Navbar = () => {
     return(
         <nav id='navbar'>
